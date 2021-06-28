65a8147e7c7ff0fe35e61849a6d24d5762f23da4
germansanz93 <germansanz93@gmail.com>
2021-06-27 22:57:56 -0300

    Final version, fixed

--- src/Board.js
+++ src/Board.js
@@ -1,38 +1,53 @@
 import React, { Component } from 'react';
 import Cell from './Cell';
-import {game, solution} from './helpers';
+import { game, solution, createGame } from './helpers';
 import './Board.css';
 
 class Board extends Component {
   static defaultProps = {
     boardSize: game.length,
   }
-  
-  constructor(props){
+
+  constructor(props) {
     super(props);
     this.state = {
       cells: game,
       solution: solution,
-      lightsRemaining: game.reduce((acc, el) => acc+el)
+      lightsRemaining: game.reduce((acc, el) => acc + el)
     }
     this.press = this.press.bind(this);
+    this.handleClick = this.handleClick.bind(this);
   }
 
-  press(btnId){
+  handleClick(){
+    let newGame = createGame();
+    let newState = {cells: newGame.game,
+      solution: newGame.solution,
+      lightsRemaining: game.reduce((acc, el) => acc + el)}
+    this.setState(newState);
+  }
+
+  press(btnId) {
     let newState = this.state;
     newState.cells[btnId] = !newState.cells[btnId];
-    if(btnId - 1 >= 0 && btnId % 5 !== 0) newState.cells[btnId - 1] = !newState.cells[btnId - 1];
-    if(btnId + 1 <= 24 && btnId % 5 !== 4) newState.cells[btnId + 1] = !newState.cells[btnId + 1];
-    if(btnId - 5 >= 0) newState.cells[btnId - 5] = !newState.cells[btnId - 5];
-    if(btnId + 5 <= 24) newState.cells[btnId + 5] = !newState.cells[btnId + 5];
-    newState.lightsRemaining = newState.cells.reduce((acc, el) => acc+el)
-    this.setState(newState)
+    if (btnId - 1 >= 0 && btnId % 5 !== 0) newState.cells[btnId - 1] = !newState.cells[btnId - 1];
+    if (btnId + 1 <= 24 && btnId % 5 !== 4) newState.cells[btnId + 1] = !newState.cells[btnId + 1];
+    if (btnId - 5 >= 0) newState.cells[btnId - 5] = !newState.cells[btnId - 5];
+    if (btnId + 5 <= 24) newState.cells[btnId + 5] = !newState.cells[btnId + 5];
+    newState.lightsRemaining = newState.cells.reduce((acc, el) => acc + el);
+    this.setState(newState);
   }
 
-  render(){
-    return(
+  render() {
+    return (
       <div className='Board'>
-        {this.state.lightsRemaining ? this.state.cells.map((el,idx) => <Cell press={this.press} value={el} key={idx} id={idx}/>) : <h2>You Win!</h2>}
+        {this.state.lightsRemaining ?
+          this.state.cells.map((el, idx) => <Cell press={this.press} value={el} key={idx} id={idx} />) :
+          <div className='endGame'>
+            <h2>You Win!</h2>
+          </div>
+        }
+        <button onClick={this.handleClick}>New game!</button>
       </div>
     )
   }
